62e2d7563b0cd6b6ed56f0ce9a02aea84b10adb3
zhaojun032 <zhaoxiu_1987@163.com>
2020-01-23 16:01:04 +0800

    死信队列

--- src/main/java/com/example/demo/config/AmqpListener.java
+++ src/main/java/com/example/demo/config/AmqpListener.java
@@ -1,27 +1,129 @@
 package com.example.demo.config;
 
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 /**
- * @Author zhaojun
+ * 死信交换机 队列监听
+ * @Author hugh
  * @create 2020/1/22 17:31
  */
 @Component
 public class AmqpListener {
+    private Logger logger = LoggerFactory.getLogger(AmqpListener.class);
+    /**
+     * 订单状态exchange
+     */
+    public static final String EXCHANGE_ORDERS = "mdd_exchange_orders_state";
+    /**
+     * 自动取消 routing_key
+     */
+    public static final String ROUTING_ORDERS_CANCEL = "mdd_routing_key_orders_cancel";
+    /**
+     * 自动签收 routing_key
+     */
+    public static final String ROUTING_ORDERS_CONFIRM = "mdd_routing_key_orders_confirm";
+    /**
+     * 自动完成 routing_key
+     */
+    public static final String ROUTING_ORDERS_FINISH = "mdd_routing_key_orders_finish";
+    /**
+     * 未支付订单queue
+     */
+    public static final String QUEUE_ORDERS_CANCEL = "mdd_queue_orders_cancel";
+    /**
+     * 自动签收queue
+     */
+    public static final String QUEUE_ORDERS_CONFIRM = "mdd_queue_orders_confirm";
+    /**
+     * 自动完成订单queue
+     */
+    public static final String QUEUE_ORDERS_FINISH = "mdd_queue_orders_finish";
 
-    @RabbitListener(queues = "tx_queue")
-    public void receive1(String message) {
-        System.err.println("tx_queue -- receive1接收到消息：" + message);
+    /**
+     * example1 手动创建队列，需要在rabbitmq后台创建queue，否则启动报错
+     * @param message
+     */
+    @RabbitListener(queues = "test_queue_1")
+    public void example1(String message) {
+        logger.info(message);
     }
 
-//    @RabbitListener(queues = "xudc.book")
-//    public void receive2(String message) {
-//        System.err.println("xudc.book -- receive2接收到消息：" + message);
-//    }
+    /**
+     * example2 自动创建队列
+     * @param message
+     */
+    @RabbitListener(queuesToDeclare = @Queue(value = "test_queue_2",durable ="true"))
+    public void example2(String message){
+        logger.info(message);
+    }
+    /**
+     * example3 自动创建队列，并且声明exchange交换机，绑定queue
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue( value = "test_queue_3"),
+            exchange = @Exchange(value = "test_exchange_1"),key = "test_routing_key_1"
+    ))
+    public void example3(String message){
+        logger.info(message);
+    }
 
-//    @RabbitListener(queues = "andy")
-//    public void receive3(String message) {
-//        System.err.println("andy -- receive3接收到消息：" + message);
-//    }
+    /**
+     *  订单自动取消queue
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue( value = QUEUE_ORDERS_CANCEL),
+            exchange = @Exchange(value = EXCHANGE_ORDERS),key = ROUTING_ORDERS_CANCEL
+    ))
+    public void autoCancel(String message){
+        System.out.println("*****");
+     //   logger.info(message);
+    }
+
+    /**
+     *  订单自动确认queue
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue( value = QUEUE_ORDERS_CONFIRM),
+            exchange = @Exchange(value = EXCHANGE_ORDERS),key = ROUTING_ORDERS_CONFIRM
+    ))
+    public void autoConfirm(String message){
+        logger.info(message);
+    }
+
+    /**
+     *  订单自动完成queue
+     * @param message
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue( value = QUEUE_ORDERS_FINISH),
+            exchange = @Exchange(value = EXCHANGE_ORDERS),key = ROUTING_ORDERS_FINISH
+    ))
+    public void autoFinish(String message){
+        logger.info(message);
+    }
+
+    /**
+     * test 死信
+     * @param message
+     * @param channel
+     * @throws IOException
+     */
+    @RabbitListener(queues = {QUEUE_ORDERS_CANCEL})
+    public void redirect(Message message, Channel channel) throws IOException {
+        //channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        logger.info("dead message  10s 后 消费消息 {}",new String (message.getBody()));
+    }
 }
--- src/main/java/com/example/demo/controller/AmqpController.java
+++ src/main/java/com/example/demo/controller/AmqpController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * @Author zhaojun
@@ -22,25 +28,41 @@ public class AmqpController {
 
     /**
      * 单点
+     *
      * @param msg
      * @return
      */
     @GetMapping("/direct")
-    public String direct(String msg){
+    public String direct(String msg) {
         rabbitTemplate.convertAndSend("amq.direct", "xudc", msg);
         return SUCCESS;
     }
 
     @GetMapping("/fanout")
     public String fanout(String msg) {
-        //rabbitTemplate.convertAndSend("amq.fanout", "", msg);
-        rabbitTemplate.convertAndSend("test_exchange_direct", "error", msg);
+        //rabbitTemplate.convertAndSend("test_exchange_direct", "error", msg);
         return SUCCESS;
     }
 
     @GetMapping("/topic")
-    public String topic(String msg){
+    public String topic(String msg) {
         rabbitTemplate.convertAndSend("amq.topic", "xudc.#", msg);
+        return SUCCESS;
+    }
+
+    @GetMapping("/dead")
+    public String deadTest(String msg) {
+        MessagePostProcessor messagePostProcessor = message -> {
+            MessageProperties messageProperties = message.getMessageProperties();
+            messageProperties.setMessageId(UUID.randomUUID().toString().replaceAll("-", ""));
+            messageProperties.setContentEncoding("utf-8");
+            //超时时间10秒
+            messageProperties.setExpiration(String.valueOf(1000 * 10));
+            return message;
+        };
+        Map<String, Object> dataMap = new HashMap<>(16);
+        dataMap.put("msg", "我是传递的消息");
+        rabbitTemplate.convertAndSend("DEAD_LETTER_EXCHANGE", "DEAD_LETTER_KEY", dataMap, messagePostProcessor);
         return SUCCESS;
     }
 }
